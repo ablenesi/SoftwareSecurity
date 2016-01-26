bd018ee2aedc93e90a9493acefe724c1e2a3db80
Blenesi Attila <ablenesi@gmail.com>
2016-01-26 18:06:26 +0200

    Lab 9 solution

--- Lab09_01/lab09_01.c
+++ Lab09_01/lab09_01.c
@@ -13,7 +13,7 @@ void CreateProcessTest()
     si.cb = sizeof(si);
     memset(&pi, 0, sizeof(pi));
 
-    CreateProcessA(NULL, "c:\\Program Files\\Windows Defender\\MSASCui.exe",
+    CreateProcessA(NULL, "\"c:\\Program Files\\Windows Defender\\MSASCui.exe\"",
         NULL, NULL,
         FALSE,
         NORMAL_PRIORITY_CLASS,
@@ -28,7 +28,7 @@ void LoadLibraryTest()
     HMODULE d3d9;
     PDirect3DCreate9 Direct3DCreate9 = NULL;
 
-    d3d9 = LoadLibraryA("d3d9.dll");
+    d3d9 = LoadLibraryExA("d3d9.dll", NULL, LOAD_LIBRARY_SEARCH_SYSTEM32);
     if (NULL == d3d9)
     {
         printf("d3d9.dll not found");
@@ -49,6 +49,46 @@ cleanup:
     FreeLibrary(d3d9);
 }
 
+int clean(char* buffer){
+    int i = 0;
+    
+    if(buffer == null){
+        return 1;
+    }
+    int size = strlen(buffer);
+
+    if(size == 0){
+        return 1;
+    }
+    
+    if( (buffer[0] == ":") ||
+        (buffer[0] == "\\" && buffer[1] == "\\")) || 
+        (buffer[0] == "\\" && buffer[1] == "." && buffer[2] == "\\")){
+        return 1;
+    }
+
+    if(size >= 2){
+        if(isalpha(buffer[0]) && buffer[1] == ":" && buffer[2] == "\\"){
+            for(int i = 5; i< size; i++){
+                if(buffer[i] == ":"){
+                    return 1;
+                }
+            }
+        }
+
+        if(size >=5){
+            if((buffer[0] == "\\" && buffer[1] == "?" && buffer[2] == "\\" && isalpha(buffer[3]) && buffer[4] == ":" && buffer[5] == "\\"){
+                for(int i = 5; i< size; i++){
+                    if(buffer[i] == ":"){
+                        return 1;
+                    }
+                }
+            }
+        }
+    }
+    return 0;
+}
+
 void CreateFileTest()
 {
     char buffer[MAX_PATH];
@@ -59,23 +99,33 @@ void CreateFileTest()
     buffer[sizeof(buffer)-1] = 0;
     getchar();
 
-    hFile = CreateFileA(
-        buffer,
-        GENERIC_READ,
-        FILE_SHARE_READ | FILE_SHARE_DELETE | FILE_SHARE_DELETE,
-        NULL,
-        OPEN_EXISTING,
-        FILE_ATTRIBUTE_NORMAL,
-        NULL);
-    if (INVALID_HANDLE_VALUE == hFile)
-    {
-        printf("The file could not be opened: %d\n", GetLastError());
-        return;
+    int i = strlen(buffer);
+    while(i>2 && (buffer[i-1] == "." || buffer[i] =="\\"){
+        i--;
     }
+    buffer[i] = "\0";
 
-    if (INVALID_HANDLE_VALUE != hFile)
-    {
-        CloseHandle(hFile);
+    if(clean(buffer) == 0 ){
+        hFile = CreateFileA(
+            buffer,
+            GENERIC_READ,
+            FILE_SHARE_READ | FILE_SHARE_DELETE | FILE_SHARE_DELETE,
+            NULL,
+            OPEN_EXISTING,
+            FILE_ATTRIBUTE_NORMAL,
+            NULL);
+        if (INVALID_HANDLE_VALUE == hFile)
+        {
+            printf("The file could not be opened: %d\n", GetLastError());
+            return;
+        }
+
+        if (INVALID_HANDLE_VALUE != hFile)
+        {
+            CloseHandle(hFile);
+        }
+    }else{
+        printf("Incorrect file path.");
     }
 }
 
